62f7b756052ac739c3d629fa209cb02c5e1b7f73
GeorgeCruz <127686075+GeoreCruz@users.noreply.github.com>
2025-08-26 22:06:46 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const mensagem = document.querySelector('#mensagem');
-const meuBotao = document.querySelector('#botao');
-
-meuBotao.addEventListener('click', function(){
-mensagem.innerHTML = 'Você clicou no botão!'
-})
